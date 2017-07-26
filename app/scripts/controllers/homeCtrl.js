f6b5ea6e9da1d5941785ca44e2e7e4fd586c8b45
Renato Gama <renatogama@Renatos-MacBook-Pro.local>
2017-07-25 22:27:21 -0300

    app ready for first test use ng click, show, model, mouseover, mouseleave and change

--- app/scripts/controllers/homeCtrl.js
+++ app/scripts/controllers/homeCtrl.js
@@ -16,6 +16,33 @@
       homeFactory.initializeApp(home);
     };
 
+    home.log = function(input) {
+      console.log(input);
+    };
+
+    home.changePriority = function(index) {
+      homeFactory.changePriority(index, home);
+    };
+
+    home.editDescription = function(index) {
+      homeFactory.editDescription(index, home);
+    };
+
+    home.saveDescription = function(index) {
+      homeFactory.saveDescription(index, home);
+    };
+
+    home.increaseSize = function(index) {
+      const allP = document.getElementsByClassName('pEdit');
+      allP[index].style.color = "blue";
+      allP[index].style.textDecoration = "underline";
+    };
+
+    home.decreaseSize = function(index) {
+      const allP = document.getElementsByClassName('pEdit');
+      allP[index].style.color = "black";
+      allP[index].style.textDecoration = "none";
+    };
 
 
   }
@@ -25,3 +52,14 @@
 
 
 })();
+
+
+// start here the flickerAPI
+    // home.flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    // home.addTag = function() {
+    //   home.flickrOptions = {tags:"",format: "json"};
+    //   home.flickrOptions.tags = home.newTagItem;
+    //   home.getJson = getJson(flickerAPI, flickrOptions);
+    //   console.log(home.flickrOptions);
+    //   home.newTagItem = "";
+    // }
--- dist/scripts/services/homeFactory.js
+++ dist/scripts/services/homeFactory.js
@@ -3,6 +3,10 @@
     homeFactory.initializeApp = function(home) {
       home.toDoList = [];
       home.newToDoItem = "";
+      home.editButton = [];
+      home.hEdit = [];
+      home.saveButton = [];
+      home.inputEdit = [];
     };
 
     homeFactory.removeItem = function(index, home) {
@@ -10,14 +14,46 @@
     };
 
     homeFactory.addNewItem = function(item, home) {
+      const index = home.toDoList.length;
       if(item !== "") {
-        home.toDoList.push(item);
+        home.toDoList.push({
+          id: index,
+          description: item,
+          priority:"",
+          status:""
+        });
         home.newToDoItem = "";
+        home.editButton[index] = true;
+        home.hEdit[index] = true;
       }
     };
 
+    homeFactory.changePriority = function(index, home) {
+      if (home.toDoList[index].priority === "high"){
+        const allRows = document.getElementsByClassName('tableRow');
+        allRows[index].style.backgroundColor = "red";
+      } else if (home.toDoList[index].priority === "medium"){
+        const allRows = document.getElementsByClassName('tableRow');
+        allRows[index].style.backgroundColor = "green";
+      } else if (home.toDoList[index].priority === "low"){
+        const allRows = document.getElementsByClassName('tableRow');
+        allRows[index].style.backgroundColor = "lightblue";
+      }
+    };
 
+    homeFactory.editDescription = function(index, home) {
+      home.editButton[index] = false;
+      home.hEdit[index] = false;
+      home.saveButton[index] = true;
+      home.inputEdit[index] = true;
+    };
 
+    homeFactory.saveDescription = function(index, home) {
+      home.editButton[index] = true;
+      home.hEdit[index] = true;
+      home.saveButton[index] = false;
+      home.inputEdit[index] = false;
+    };
 
     return homeFactory;
   }
